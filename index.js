d489b7df5a716feeed22e2f9602445d3c6a6c504
unknown <kikusko123456789@gmail.com>
2023-01-02 10:00:44 +0100

    Lol

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const expressApp = express();
+const axios = require('axios');
+const path = require('path');
+const port = process.env.port || 3000;
+
+expressApp.use(express.static('static'))
+expressApp.use(express.json());
+
+require('dotenv').config();
+
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+expressApp.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+bot.launch()
